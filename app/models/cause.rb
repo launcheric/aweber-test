9f62b25fccb740f83db2dbfb3e4944633e0e040d
Cassandra King <puddincat@gmail.com>
2018-04-21 17:22:12 -0400

    update with routes, controllers

--- app/models/cause.rb
+++ app/models/cause.rb
@@ -1,5 +1,9 @@
 class Cause < ApplicationRecord
   belongs_to :user
+  has_many :events
+  has_many :articles
+  has_many :cause_interests
+  has_many :users, through: :cause_interests
 
   validates :name, presence: true
   validates :description, presence: true
--- config/routes.rb
+++ config/routes.rb
@@ -4,10 +4,20 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get 'session/create'
-
   get 'session/destroy'
 
   get 'auth/developer', :as => 'developer_auth'
   match 'auth/:provider/callback' => 'session#create', :via => [:get, :post]
 
+end
+
+Rails.application.routes.draw do
+namespace :api do
+    namespace :v1 do
+      resources :causes do
+        resources :events
+        resources :articles
+      end
+    end
+  end
 end
